496f23896869aa4f79f6dbe668f8e290712c393a
Savon Mctair <savonmctair20@gmail.com>
2026-03-04 21:43:26 -0500

    Add files via upload

--- Week 4/main.cpp
+++ Week 4/main.cpp
@@ -4,42 +4,45 @@ using namespace std;
 /*Name: Savon Mctair
 Course: CSCI 272
 Lesson Number : Lesson 4 
-Code Topic: Encapsulation Bank  Example
+Code Topic: Encapsulation Example
 Date : 2/3/2026
 */
-class BankAccount {
+class User{
     private:
-    double balance{0};
+    string username;
+    string password;
     public:
-    void deposit(double amount){
-        balance += amount;
+    User(const string& name, const string& pwd){
+        username = name;
+        password = pwd;
     }
-    void withdraw(double amount){
-        if(amount <= balance){
-            balance -= amount;
-        }
+    bool login(const string& inputUser, const string& inputPwd) const{
+        return (inputUser == username) && (inputPwd == password);
     }
-     void displayBalance(){
-         cout << "Current balance: $"<< balance << endl;
-     }
+    void updatePassword(const string& newPwd){
+        password = newPwd;
+    }
 };
 int main(){
-    BankAccount account;
-    account.deposit(500);
-    account.withdraw(200);
-    account.displayBalance();
-    return 0;
-}
-
+    User user1("admin","1234");
+    if(user1.login("admin", "1234")){
+        cout << "Login successful!"<< endl;
+    } else {
+        cout << "Login failed!"<< endl;
+    }
+     user1.updatePassword("new password");
+     return 0;
+    }
 /*
 1.What I understood:
 What I understood was to make a break down with Encapsulation as it make see how to use it in a real life moment
 2. Difficulties:
-I had error with making the numbers print out as it gives me an error on the class but I fix it.
+Not much Difficulties i was irritaed on these if/ else statements.
 3. Research:
-I re-looked at the slides and try to make it understand for me
+I re-looked at the slides and try to make it understand for me.
 4. AI usage:
-I use AI to understand this concpet. 
+I use Claude to understand this concpet. 
 5. What I learned:
-I learned that Encapsulation can use on objects
+I learned that Encapsulation can use on objects.
 */
+    
